edf61a47dcb2376b78fe6ec2056b273878244f9a
Antony Budianto <antonybudianto@gmail.com>
2017-08-30 08:14:56 +0700

    add missing semicolon

--- templates/server/src/index.js
+++ templates/server/src/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`CRA Server listening on port ${PORT}!`)
+  console.log(`CRA Server listening on port ${PORT}!`);
 });
