2a6d0e960dea6e4d6addcc84865ff4f3b33c8d8d
Antony Budianto <antonybudianto@gmail.com>
2017-10-01 16:32:29 +0700

    init clean command

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ program
   .version('0.1.0')
   .command('init', 'Initialize your CRA server')
   .command('build', 'Build both client and server')
+  .command('clean', 'Clean build result')
   .parse(process.argv);
