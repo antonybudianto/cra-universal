4f285e6ee0f2c814a285cb6b866fa99527bd33db
Antony Budianto <antonybudianto@gmail.com>
2018-06-22 11:37:15 +0700

    update clean command

--- src/clean.js
+++ src/clean.js
@@ -5,24 +5,22 @@ var rimraf = require('rimraf');
 var chalk = require('chalk');
 
 const cwd = process.cwd();
-const paths = [
-  'build',
-  'dist',
-  'server/build'
-];
+const paths = ['build', 'dist', 'server-build'];
 
-inquirer.prompt([
-  {
-    type: 'confirm',
-    message: 'Are you sure to clean the build result?',
-    name: 'ok'
-  }
-]).then(function (answers) {
-  if (answers.ok) {
-    paths.forEach(path => {
-      rimraf(`${cwd}/${path}`, (err) => {
-        console.log(`${chalk.bgWhite(path)} cleaned!`);
+inquirer
+  .prompt([
+    {
+      type: 'confirm',
+      message: 'Are you sure to clean the build result?',
+      name: 'ok'
+    }
+  ])
+  .then(function(answers) {
+    if (answers.ok) {
+      paths.forEach(path => {
+        rimraf(`${cwd}/${path}`, err => {
+          console.log(`${chalk.bgWhite(path)} cleaned!`);
+        });
       });
-    });
-  }
-});
+    }
+  });
