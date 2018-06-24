abca75cfd54d9cbb7c8c5f4e281d8b4ed604139d
Antony Budianto <antonybudianto@gmail.com>
2018-06-24 14:44:25 +0700

    refactor templates to use new crau

--- templates/server/app.js
+++ templates/server/app.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 const React = require('react');
-import { createReactAppExpress, handleUniversalRender } from '@cra-express/core';
+import {
+  createReactAppExpress,
+  handleUniversalRender
+} from '@cra-express/core';
 
-const {default: App} = require('../../src/App');
+const { default: App } = require('../src/App');
 const clientBuildPath = path.resolve(__dirname, 'client');
 const app = createReactAppExpress({
   clientBuildPath,
