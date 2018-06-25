6b65270e6d7e34569089218f842c459a153ec23c
Antony Budianto <antonybudianto@gmail.com>
2018-06-25 20:19:54 +0700

    add hmr support for default server

--- src/config/server/app.js
+++ src/config/server/app.js
@@ -1,15 +1,27 @@
-import path from 'path';
-import React from 'react';
+const path = require('path');
+
+const React = require('react');
 import {
   createReactAppExpress,
   handleUniversalRender
 } from '@cra-express/core';
-import App from 'appbase/src/App';
 
-const clientBuildPath = path.resolve(__dirname, '../client');
+const { default: App } = require('appbase/src/App');
+const clientBuildPath = path.resolve(__dirname, 'client');
+
+let AppEl = App;
+
 const app = createReactAppExpress({
   clientBuildPath,
-  universalRender: handleUniversalRender(<App />)
+  universalRender: () => handleUniversalRender(<AppEl />)()
 });
+
+if (module.hot) {
+  module.hot.accept('appbase/src/App', () => {
+    const { default: App } = require('appbase/src/App');
+    AppEl = App;
+    console.log('✅ Server hot reloaded App');
+  });
+}
 
 module.exports = app;
