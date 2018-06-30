fbf4ad87b2fd82500d40fcadcd6e9f2b72237eb5
Antony Budianto <antonybudianto@gmail.com>
2018-06-30 17:49:26 +0700

    clean build before start

--- src/cli/start.js
+++ src/cli/start.js
@@ -1,39 +1,49 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const webpack = require('webpack');
+const del = require('del');
+const { log } = require('../util/log');
 
-const config = require('../config/webpack.config');
+function cleanBuild() {
+  log('Cleaning build...');
+  return del(['server-build']).then(() => {
+    log('Build clean done.');
+  });
+}
 
-const ins = webpack(config);
-ins.watch(
-  {
-    ignored: /node_modules/,
-    aggregateTimeout: 300,
-    poll: undefined
-  },
-  (err, stats) => {
-    if (err) {
-      console.error(err.stack || err);
-      if (err.details) {
-        console.error(err.details);
+cleanBuild().then(() => {
+  const config = require('../config/webpack.config');
+  const ins = webpack(config);
+  ins.watch(
+    {
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: undefined
+    },
+    (err, stats) => {
+      if (err) {
+        console.error(err.stack || err);
+        if (err.details) {
+          console.error(err.details);
+        }
+        return;
       }
-      return;
-    }
 
-    const info = stats.toJson();
+      const info = stats.toJson();
 
-    if (stats.hasErrors()) {
-      console.error(info.errors);
-    }
+      if (stats.hasErrors()) {
+        console.error(info.errors);
+      }
 
-    if (stats.hasWarnings()) {
-      console.warn(info.warnings);
+      if (stats.hasWarnings()) {
+        console.warn(info.warnings);
+      }
+      console.log(
+        stats.toString({
+          modules: false,
+          colors: true // Shows colors in the console
+        })
+      );
     }
-    console.log(
-      stats.toString({
-        modules: false,
-        colors: true // Shows colors in the console
-      })
-    );
-  }
-);
+  );
+});
