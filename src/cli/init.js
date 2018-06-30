9e55b645b3f513ffb888609025fb1e31eed11871
Antony Budianto <antonybudianto@gmail.com>
2018-06-30 14:41:15 +0700

    Implement install using yarn

--- src/cli/init.js
+++ src/cli/init.js
@@ -4,16 +4,24 @@ const path = require('path');
 const fs = require('fs-extra');
 const chalk = require('chalk');
 const { spawnSync } = require('child_process');
+const hasYarn = require('has-yarn');
 
 const { log } = require('../util/log');
 const src = path.resolve(__dirname, '../..', 'templates', 'server');
 const dest = path.resolve(process.cwd(), 'server');
 const isWindows = process.platform === 'win32';
 const npm = isWindows ? 'npm.cmd' : 'npm';
+const hasYarnFlag = hasYarn(path.resolve(process.cwd()));
+const command = hasYarnFlag ? 'yarn' : 'npm';
+const args = hasYarnFlag
+  ? ['add', '@cra-express/core@2.2.5']
+  : ['i', '-S', '@cra-express/core@2.2.5'];
 
 function installDep() {
-  log('Adding required dependencies...');
-  const result = spawnSync(npm, ['i', '-S', '@cra-express/core@2.2.5'], {
+  log(
+    `Installing required dependencies using ${hasYarnFlag ? 'yarn' : 'npm'}...`
+  );
+  const result = spawnSync(command, args, {
     stdio: 'inherit'
   });
   if (result.status !== 0) {
