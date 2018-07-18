fe3e3e340b1a706de4b5f258602516502600ae5a
Antony Budianto <antonybudianto@gmail.com>
2018-07-18 19:59:21 +0700

    Remove handleUniversalRender helper

--- packages/@cra-express/core/src/handle-universal-render.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const { renderToNodeStream } = require('react-dom/server');
-
-function handleUniversalRender(reactElement) {
-  return (req, res) => renderToNodeStream(reactElement);
-}
-
-export default handleUniversalRender;
