ec720693ad7a56efd43412af3a45516d7ed31dea
Antony Budianto <antonybudianto@gmail.com>
2018-07-29 14:51:47 +0700

    init footer content

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -35,30 +35,32 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('intro-getting-started.html', this.props.language)}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('guide-deployment.html', this.props.language)}>
+              Deployment Guide
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href={this.docUrl('cra-universal-cli.html', this.props.language)}>
+              API Reference
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
             <a
-              href="http://stackoverflow.com/questions/tagged/"
+              href="http://stackoverflow.com/questions/tagged/cra-universal"
               target="_blank"
               rel="noreferrer noopener">
               Stack Overflow
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
             <a
-              href="https://twitter.com/"
+              href="https://spectrum.chat/cra-universal"
+              target="_blank"
+              rel="noreferrer noopener">
+              Spectrum
+            </a>
+            <a
+              href="https://twitter.com/antonybudianto"
               target="_blank"
               rel="noreferrer noopener">
               Twitter
