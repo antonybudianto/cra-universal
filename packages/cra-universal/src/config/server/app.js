921faaa8a669026995123a0940c9960074ea337f
Antony Budianto <antonybudianto@gmail.com>
2018-09-09 16:05:15 +0700

    fix export on default cra server

--- packages/cra-universal/src/config/server/app.js
+++ packages/cra-universal/src/config/server/app.js
@@ -24,4 +24,4 @@ if (module.hot) {
   });
 }
 
-module.exports = app;
+export default app;
--- packages/cra-universal/src/config/server/index.js
+++ packages/cra-universal/src/config/server/index.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const app = require('./app').default;
 
 const PORT = process.env.CRA_SERVER_PORT || 3001;
 
