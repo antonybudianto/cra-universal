f7eff6cb3c0ad29a6eb05d6d292a72acd8ba4b64
Antony Budianto <antonybudianto@gmail.com>
2018-11-11 10:04:24 +0700

    update cra-universal dependencies (#124)

--- packages/cra-universal/templates/src/App.js
+++ packages/cra-universal/templates/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Jono</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
