0b4cf0d05110cbb3fa4cd1d0fbe8658e07fc0e22
Antony Budianto <antonybudianto@gmail.com>
2018-12-01 09:32:34 +0700

    fix babel dependencies

--- packages/babel-preset-cra-universal/index.js
+++ packages/babel-preset-cra-universal/index.js
@@ -2,44 +2,44 @@
 
 var preset = {
   presets: [
-    [require.resolve('babel-preset-env'), { modules: false }],
-    require.resolve('babel-preset-react'),
+    [require.resolve('@babel/preset-env'), { modules: false }],
+    require.resolve('@babel/preset-react')
   ],
   plugins: [
     // class { handleThing = () => { } }
-    require.resolve('babel-plugin-transform-class-properties'),
+    require.resolve('@babel/plugin-proposal-class-properties'),
 
     // The following two plugins use Object.assign directly, instead of Babel's
     // extends helper. Note that this assumes `Object.assign` is available.
     // { ...todo, completed: true }
     [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
+      require.resolve('@babel/plugin-proposal-object-rest-spread'),
       {
-        useBuiltIns: true,
-      },
+        useBuiltIns: true
+      }
     ],
     // Adds syntax support for import()
-    require.resolve('babel-plugin-syntax-dynamic-import'),
+    require.resolve('@babel/plugin-syntax-dynamic-import'),
     // Add support for async/await
-    require.resolve('babel-plugin-transform-runtime'),
-  ],
+    require.resolve('@babel/plugin-transform-runtime')
+  ]
 };
 
 var env = process.env.BABEL_ENV || process.env.NODE_ENV;
 if (env !== 'development' && env !== 'test' && env !== 'production') {
   throw new Error(
     'Using `babel-preset-cra-universal` requires that you specify `NODE_ENV` or ' +
-    '`BABEL_ENV` environment variables. Valid values are "development", ' +
-    '"test", and "production". Instead, received: ' +
-    JSON.stringify(env) +
-    '.'
+      '`BABEL_ENV` environment variables. Valid values are "development", ' +
+      '"test", and "production". Instead, received: ' +
+      JSON.stringify(env) +
+      '.'
   );
 }
 
 if (env === 'development' || env === 'test') {
   preset.plugins.push.apply(preset.plugins, [
     // Adds component stack to warning messages
-    require.resolve('babel-plugin-transform-react-jsx-source'),
+    require.resolve('@babel/plugin-transform-react-jsx-source')
   ]);
 }
 
@@ -49,15 +49,15 @@ if (env === 'test') {
     require.resolve('babel-plugin-dynamic-import-node'),
     // Transform ES modules to commonjs for Jest support
     [
-      require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
-      { loose: true },
-    ],
+      require.resolve('@babel/plugin-transform-modules-commonjs'),
+      { loose: true }
+    ]
   ]);
 }
 
 if (env === 'production') {
   preset.plugins.push.apply(preset.plugins, [
-    require.resolve('babel-plugin-transform-react-remove-prop-types'),
+    require.resolve('babel-plugin-transform-react-remove-prop-types')
   ]);
 }
 
