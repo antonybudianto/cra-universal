37b7ea8cf43c1f91e8314fb5a3283c07b84ae982
Alan Chen <alan@MBP.local>
2019-01-09 15:58:41 -0800

    add beforeStaticLoader as option

--- packages/@cra-express/core/src/app.js
+++ packages/@cra-express/core/src/app.js
@@ -4,6 +4,9 @@ const universalLoader = require('@cra-express/universal-loader').default;
 
 function createReactAppExpress (options) {
   const app = express();
+  if (options.beforeStaticLoader) {
+    options.beforeStaticLoader(app, options);
+  }
   staticLoader(app, options);
   universalLoader(app, options);
   return app;
