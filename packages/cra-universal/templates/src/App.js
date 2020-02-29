58bf03dc2dc024c868dda9b5297eb05d033859ec
Antony Budianto <antonybudianto@gmail.com>
2020-02-29 18:32:01 +0700

    feat: setup svg support for ssr

--- packages/cra-universal/templates/src/App.js
+++ packages/cra-universal/templates/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import { ReactComponent as ReactSVG } from './react.svg';
 
 class App extends Component {
   render() {
@@ -8,6 +10,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
+        <ReactSVG width={200} height={200} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
